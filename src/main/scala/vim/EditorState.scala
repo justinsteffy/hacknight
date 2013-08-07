81ec9ffc238b95e8309734dbca2f396ae614152a
Justin Steffy <steffy@orgsync.com>
2013-08-07 15:10:10 -0500

    remove stuff not related to pages

--- src/main/scala/vim/EditorState.scala
+++
@@ -1,8 +0,0 @@
-package vim
-
-case class EditorState(val content: Seq[String], val position: (Int, Int)) {
-    def line = content(row)
-  def row = position._1
-  def col = position._2
-}
-
--- src/main/scala/vim/package.scala
+++
@@ -1,5 +0,0 @@
-package object vim {
-  
-  type CommandExec = EditorState => EditorState
-
-}
